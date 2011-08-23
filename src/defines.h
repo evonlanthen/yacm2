b573b0cf441bc0f819f3f7418fca4b742cbb42b0
Elmar Vonlanthen <elmi78@gmx.ch>
2011-08-23 21:05:16 +0200

    File with common defines added (TRUE/FALSE, Board, ...)

---
+++ src/defines.h
@@ -0,0 +1,42 @@
+/**
+ * @brief   Preprocessor definitions
+ * @file    defines.h
+ * @version 1.0
+ * @authors	Toni Baumann (bauma12@bfh.ch), Ronny Stauffer (staur3@bfh.ch), Elmar Vonlanthen (vonle1@bfh.ch)
+ * @date    May 20, 2011
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+/**
+ * Define the BOARD to use (CARME or ORCHID)
+ */
+//#define CARME
+#ifndef CARME
+	#define ORCHID
+#endif
+
+
+/**
+ * Define symbol for the boolean value 'true'.
+ */
+#define TRUE 1
+/**
+ * Define symbol for the boolean value 'false'.
+ */
+#define FALSE 0
+
+/**
+ * Define NULL pointer
+ */
+#ifndef NULL
+ #define NULL ((void*)0)
+#endif
+
+/**
+ * Define end of file value
+ */
+#define EOF (-1)
+
+#endif /* DEFINES_H_ */
